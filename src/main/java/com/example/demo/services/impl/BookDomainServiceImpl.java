2b2737e1a2840ce8a6b4b1fc110bb39600938c02
samuelmsilva2v <samuelmsilva@outlook.com.br>
2024-12-16 14:44:00 -0300

    service layer

---
+++ src/main/java/com/example/demo/services/impl/BookDomainServiceImpl.java
@@ -0,0 +1,46 @@
+package com.example.demo.services.impl;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dtos.BookRequestDto;
+import com.example.demo.dtos.BookResponseDto;
+import com.example.demo.services.interfaces.BookDomainService;
+
+@Service
+public class BookDomainServiceImpl implements BookDomainService {
+
+	@Override
+	public BookResponseDto register(BookRequestDto request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BookResponseDto update(UUID id, BookRequestDto request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String delete(UUID id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BookResponseDto getById(UUID id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<BookResponseDto> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
--- src/main/java/com/example/demo/services/interfaces/BookDomainService.java
+++ src/main/java/com/example/demo/services/interfaces/BookDomainService.java
@@ -1,6 +1,20 @@
 package com.example.demo.services.interfaces;
 
+import java.util.List;
+import java.util.UUID;
+
+import com.example.demo.dtos.BookRequestDto;
+import com.example.demo.dtos.BookResponseDto;
+
 public interface BookDomainService {
 
+	public BookResponseDto register(BookRequestDto request);
 	
+	public BookResponseDto update(UUID id, BookRequestDto request);
+	
+	public String delete(UUID id);
+	
+	public BookResponseDto getById(UUID id);
+	
+	public List<BookResponseDto> getAll();
 }
